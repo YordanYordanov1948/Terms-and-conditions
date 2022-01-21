0a25916f8ff068602536296943c1a5a62fe5ac76
Yordan <yordan_yordanov001@abv.bg>
2022-01-21 13:44:55 +0200

    fix

--- src/Document.js
+++ src/Document.js
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export default function Document({ title, content }) {
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    const onScroll = () => setOffset(window.pageYOffset);
-    window.removeEventListener("scroll", onScroll);
-    window.addEventListener("scroll", onScroll, { passive: true });
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-
-  console.log(offset);
+export default function Document() {
   return (
     <div className="content">
       <div className="title">Terms and Conditions</div>
-      <button type="button" disabled onClick={setOffset}>
+      <button type="button" disabled>
         I Agree
       </button>
-      {title}
-      {content}
     </div>
   );
 }
