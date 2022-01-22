ee882de222461f2aa39421be9026802ed51017eb
Yordan <yordan_yordanov001@abv.bg>
2022-01-22 08:34:10 +0200

    solution

--- src/Document.js
+++ src/Document.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
 export default function Document(props) {
-  const [value, setValue] = useState("");
+  const [value, setDisable] = useState(false);
+
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) {
+      setDisable(true);
+    }
+  };
 
   return (
-    <div className="content">
+    <div className="content" onClick={handleScroll}>
       {props.content}
       <div className="title">{props.title}</div>
       <button disabled={!value}>I Agree</button>{" "}
